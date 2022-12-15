16bce1dd497d5809096793ed94f79e80df7d64b2
frendzi5 <120679372+frendzi5@users.noreply.github.com>
2022-12-15 20:17:18 +0100

    Update 10naj.php

--- 10naj.php
+++ 10naj.php
@@ -5,10 +5,7 @@
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
   }
-
   $sql = "SELECT * FROM ksiazki ORDER BY data_dodania DESC LIMIT 10; ";
-
-
   if ($result = mysqli_query($con, $sql)) {
     echo '<table><tr><th>Autor</th><th id="nazwa">Tytuł</th></tr>';
     while ($row = mysqli_fetch_row($result)) {
@@ -17,6 +14,5 @@
     echo '</table>';
     mysqli_free_result($result);
   }
-
   mysqli_close($con);
    ?>
